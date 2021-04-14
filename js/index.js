3c999ab9a6a36de94a70cae77b5fd7f353ec4b15
Nahum <losond8@gmail.com>
2021-04-14 13:18:50 -0400

    Fetched data from sever, currently displaying data on the page via DOM manipulation, used vanilla JS to build form.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,52 @@
-//Fetch the montsters
-//GET http://localhost:3000/monsters/?_limit=20&_page=3
-
-const URL_PREFIX='http://localhost:3000/';
 let page = 1;
+const URL_PREFIX='http://localhost:3000/'
+
+
 const getMonsters = (a) => {
     fetch (URL_PREFIX + `monsters/?_limit=50&_pages=${a}`)
     .then(resp => resp.json())
-    .then (monsters => {
+    .then (newMonster => {
         //console.log(data)
-        monsters.forEach(monster => buildMonster(monster))})
+        newMonster.forEach(monster => {
+            buildMonster(monster)})
+    })
 }
-getMonsters("a")
+getMonsters()
+
+//Building our Form 
+const buildForm = () => {
+   const container = document.querySelector('#monster-container')
+   const form = document.createElement('form')
+   let name = document.createElement('input')
+   name.setAttribute('type', 'text')
+   name.setAttribute('name', 'name')
+   name.setAttribute('placeholder', 'name')
+
+   let age = document.createElement('input')
+   age.setAttribute('type', 'text')
+   age.setAttribute('name', 'age')
+   age.setAttribute('placeholder', 'age')
+
+   let description = document.createElement('input')
+   description.setAttribute('type', 'text')
+   description.setAttribute('name', 'description')
+   description.setAttribute('placeholder', 'description')
+
+   //create submit button
+   let submit = document.createElement('input')
+   submit.textContent= "Create"
+   submit.setAttribute('type', 'submit')
+   submit.setAttribute('value', 'Submit')
+
+   form.append(name, age, description, submit)
+
+   container.appendChild(form)
+}
+buildForm()
 
 //Display data on the page
 buildMonster = (monster) => {
+
     let div = document.querySelector('#monster-container')
     let h2 = document.createElement('h2')
     let h3 = document.createElement('h3')
@@ -24,4 +57,17 @@ buildMonster = (monster) => {
     p.textContent = monster.description
 
     div.append(h2, h3, p)
+
 }
+
+// const handleAddMonster = (monsterObj) =>{
+//     fetch ( 'http://localhost:3000/monsters', {
+//         method: 'POST', 
+//         headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json"
+//         },
+//         body: JSON.stringify({
+            
+//         })
+//     })
